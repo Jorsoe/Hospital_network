1e77e4b838ceb465223b24125f3e61eb85172516
Jorsoe <sjrArk9939>
2021-11-25 12:35:24 +0800

    为项目增加与数据库对应实体类与启动入口

---
+++ service/service_hosp/src/main/java/com/xatu/jorsoe/hospital/ServiceHospitalApplication.java
@@ -0,0 +1,18 @@
+package com.xatu.jorsoe.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Renke
+ * @DateTime: 2021/11/25 11:33
+ * @Description: TODO 医院挂号项目启动类
+ */
+
+@SpringBootApplication
+public class ServiceHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospitalApplication.class, args);
+    }
+}
