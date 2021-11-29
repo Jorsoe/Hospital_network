831f24740a30086bbf68f4d707edc91b254b5447
Jorsoe <sjrArk9939>
2021-11-29 17:08:37 +0800

    增添关于医院设置相关的业务逻辑接口

--- service/service_hosp/src/main/java/com/xatu/jorsoe/hospital/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/xatu/jorsoe/hospital/controller/HospitalSetController.java
@@ -1,19 +1,25 @@
 package com.xatu.jorsoe.hospital.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xatu.jorsoe.hospital.model.hosp.HospitalSet;
 import com.xatu.jorsoe.hospital.service.HospitalSetService;
+import com.xatu.jorsoe.hospital.utils.MD5;
+import com.xatu.jorsoe.hospital.vo.hosp.HospitalSetQueryVo;
 import com.xatu.jorsoe.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * @Author: Renke
  * @DateTime: 2021/11/25 14:13
- * @Description: TODO 医院配置Web
+ * @Description: TODO 医院配置访问处理器
  */
 
 @Api(tags = "医院配置管理")
@@ -26,19 +32,80 @@ public class HospitalSetController {
 
     @ApiOperation(value = "获取所有医院设置信息")
     @GetMapping("findAll")
-    public Result getHospitalSet(){
+    public Result getHospitalSet() {
         List<HospitalSet> list = hospitalSetService.list();
         return Result.ok(list);
     }
 
     @ApiOperation(value = "逻辑删除医院配置")
     @DeleteMapping("{id}")
-    public Result removeHospital(@PathVariable Long id){
+    public Result removeHospital(@PathVariable Long id) {
         boolean flag = hospitalSetService.removeById(id);
         if (flag){
             return Result.ok();
         } else {
             return Result.fail();
         }
+    }
+
+    @ApiOperation(value = "条件查询带分页医院设置信息")
+    @PostMapping("findPage/{current}/{limit}")
+    public Result findPageHospital(@PathVariable Long current,
+                                   @PathVariable Long limit,
+                                   @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+        Page<HospitalSet> page = new Page<>(current, limit);
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+
+        String hosname = hospitalSetQueryVo.getHosname();
+        String hoscode = hospitalSetQueryVo.getHoscode();
+        if (!StringUtils.isEmpty(hosname)) {
+            wrapper.like("hosname", hospitalSetQueryVo.getHosname());
+        }
+        if (!StringUtils.isEmpty(hoscode)) {
+            wrapper.like("hoscode", hospitalSetQueryVo.getHoscode());
+        }
+        Page<HospitalSet> hospitalSetPage = hospitalSetService.page(page, wrapper);
+        return Result.ok(hospitalSetPage);
+    }
+
+    @ApiOperation(value = "添加医院设置")
+    @PostMapping("saveHospitalSet")
+    public Result saveHospitalSet(@RequestBody HospitalSet hospitalSet) {
+        //状态:1/0 能/不能
+        hospitalSet.setStatus(1);
+        //签名密钥
+        Random random = new Random();
+        hospitalSet.setSignKey(MD5.encrypt(System.currentTimeMillis()+""+random.nextInt(1000)));
+        boolean save = hospitalSetService.save(hospitalSet);
+        if (save){
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation(value = "根据id获取医院设置")
+    @GetMapping("getHospitalSet/{id}")
+    public Result getHospitalSet(@PathVariable long id) {
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        return Result.ok(hospitalSet);
+    }
+
+    @ApiOperation(value = "修改医院设置")
+    @PostMapping("updateHospitalSet")
+    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet) {
+        boolean update = hospitalSetService.updateById(hospitalSet);
+        if (update) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("批量删除医院设置")
+    @DeleteMapping("batchRemove")
+    public Result batchRemoveHospitalSet(@RequestBody List<Long> idList){
+        hospitalSetService.removeByIds(idList);
+        return Result.ok();
     }
 }
